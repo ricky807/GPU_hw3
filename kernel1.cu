f8a91ae15a43260dd637d54709fa49efbfdaa082
eortiz5 <eortiz5@cscd-gpu01.eastern.ewu.edu>
2021-11-04 10:21:19 -0700

    Glory

--- kernel1.cu
+++ kernel1.cu
@@ -26,44 +26,55 @@ __global__ void k1( float* g_dataA, float* g_dataB, int floatpitch, int width)
 
     int shared_i_index = threadIdx.y + 1;
     int shared_j_index = threadIdx.x + 1;
-    if(i < width - 1|| j < width - 1 || i >= 1 || j >= 1)
+    if(i < width - 1|| j < width - 1)
     {
-        
         s_data[(shared_i_index-1) * blocksize + shared_j_index] = g_dataA[(i-1) * floatpitch + j];
         s_data[shared_i_index * blocksize + shared_j_index] = g_dataA[i * floatpitch + j];
         s_data[(shared_i_index+1) * blocksize + shared_j_index] = g_dataA[(i+1) * floatpitch + j];
         
         if(shared_j_index == 1)
         {
-            s_data[(shared_i_index-1) * blocksize + shared_j_index-1] = g_dataA[i * floatpitch + j-1];
-            s_data[shared_i_index * blocksize + shared_j_index-1] = g_dataA[(i+1) * floatpitch + j-1];
-            s_data[(shared_i_index+1) * blocksize + shared_j_index-1] = g_dataA[(i+2) * floatpitch + j-1];
+            s_data[(shared_i_index-1) * blocksize + shared_j_index-1] = g_dataA[(i-1) * floatpitch + j-1];
+            s_data[shared_i_index * blocksize + shared_j_index-1] =  g_dataA[i * floatpitch + j-1];
+            s_data[(shared_i_index+1) * blocksize + shared_j_index-1] = g_dataA[(i+1) * floatpitch + j-1];
         }
         if(shared_j_index == blocksize -2)
         {
-            s_data[(shared_i_index-1) * blocksize + shared_j_index+1] = g_dataA[i * floatpitch + j+1];
-            s_data[shared_i_index * blocksize + shared_j_index+1] = g_dataA[(i+1) * floatpitch + j+1];
-            s_data[(shared_i_index+1) * blocksize + shared_j_index+1] = g_dataA[(i+2) * floatpitch + j+1];
+            s_data[(shared_i_index-1) * blocksize + shared_j_index+1] = g_dataA[(i-1) * floatpitch + j+1];
+            s_data[shared_i_index * blocksize + shared_j_index+1] = g_dataA[i * floatpitch + j+1];
+            s_data[(shared_i_index+1) * blocksize + shared_j_index+1] = g_dataA[(i+1) * floatpitch + j+1];
         }
     }
     __syncthreads();
-
-    if( i >= width - 1|| j >= width - 1 || i < 1 || j < 1 ) return;
-    g_dataB[i * floatpitch + j] = s_data[i * blocksize + j];
-
     /*
-    g_dataB[i * floatpitch + j] = (
-                                0.2f * s_data[i * blocksize + j] +               //itself
-                                0.1f * s_data[(i-1) * blocksize +  j   ] +       //N
-                                0.1f * s_data[(i-1) * blocksize + (j+1)] +       //NE
-                                0.1f * s_data[ i    * blocksize + (j+1)] +       //E
-                                0.1f * s_data[(i+1) * blocksize + (j+1)] +       //SE
-                                0.1f * s_data[(i+1) * blocksize +  j   ] +       //S
-                                0.1f * s_data[(i+1) * blocksize + (j-1)] +       //SW
-                                0.1f * s_data[ i    * blocksize + (j-1)] +       //W
-                                0.1f * s_data[(i-1) * blocksize + (j-1)]         //NW
-                            ) * 0.95f; 
+    if(blockIdx.x == 2 && blockIdx.y == 13 && threadIdx.x == 0 && threadIdx.y == 0)
+    {
+        for(int u = 0; u < 3; u++)
+        {
+            for(int y = 0; y < blocksize; y++)
+            {
+                printf("%04.2f ", s_data[u*blocksize + y]);
+            }
+            printf("\n");
+        }
+        printf("\n");
+    }
     */
+    if( i >= width - 1|| j >= width - 1 || i < 1 || j < 1 ) return;
+    
+    
+    g_dataB[i * floatpitch + j] = (
+                                0.2f * s_data[shared_i_index * blocksize + shared_j_index] +               //itself
+                                0.1f * s_data[(shared_i_index-1) * blocksize +  shared_j_index   ] +       //N
+                                0.1f * s_data[(shared_i_index-1) * blocksize + (shared_j_index+1)] +       //NE
+                                0.1f * s_data[ shared_i_index    * blocksize + (shared_j_index+1)] +       //E
+                                0.1f * s_data[(shared_i_index+1) * blocksize + (shared_j_index+1)] +       //SE
+                                0.1f * s_data[(shared_i_index+1) * blocksize +  shared_j_index   ] +       //S
+                                0.1f * s_data[(shared_i_index+1) * blocksize + (shared_j_index-1)] +       //SW
+                                0.1f * s_data[ shared_i_index    * blocksize + (shared_j_index-1)] +       //W
+                                0.1f * s_data[(shared_i_index-1) * blocksize + (shared_j_index-1)]         //NW
+                            ) * 0.95f; 
+    
     
 }
 
